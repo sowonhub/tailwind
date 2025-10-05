a5ec8abace77730d68ff81421d8a371740c0ff16
kimsowon <sowon836@gmail.com>
2025-10-06 00:30:13 +0900

    React 12일차 과제제출

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center p-[10px_20px] bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="flex justify-center items-center gap-[20px] list-none">
+        <li className="font-normal text-sm">로그인</li>
+        <li className="font-normal text-sm">회원가입</li>
+        <li className="font-normal text-sm">내클래스</li>
       </ul>
     </header>
   );
